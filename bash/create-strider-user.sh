016f28ad7a315cf920b18382afd12dcbf7e1b907
Raymond Kao <rakao@microsoft.com>
2017-03-02 18:57:48 -0500

    create a random password hash for user

--- bash/create-strider-user.sh
+++ bash/create-strider-user.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 sudo -i
+RANDOM_PASSWORD_HASH=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 adduser strider --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
-echo "strider:password" | chpasswd
+echo "strider:$RANDOM_PASSWORD_HASH" | chpasswd
 exit
