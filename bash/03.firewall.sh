4ef22baed2b8ead1640c75470b91ad97145a974d
Raymond Kao <rakao@microsoft.com>
2017-03-03 00:47:40 -0500

    updated folder structure

--- bash/03.firewall.sh
+++ bash/03.firewall.sh
@@ -5,7 +5,7 @@ sudo -i
 # allow mongo from localhost only
 ufw allow from 127.0.0.1 to 127.0.0.1 port 27017 proto tcp
 
-# allow http
-ufw allow http
+# allow node/strider default
+ufw allow 3000
 
 ufw enable
--- bash/04.install-mongodb.sh
+++ bash/04.install-mongodb.sh
@@ -6,10 +6,7 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A145185
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 apt-get update
 apt-get install -y mongodb-org
-
 cd /etc/systemd/system/
 wget https://raw.githubusercontent.com/raykao/strider-azure-deployment/master/config/mongodb.service
-
 systemctl start mongodb
-
 systemctl enable mongodb
