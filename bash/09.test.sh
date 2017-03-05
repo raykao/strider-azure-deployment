9dc8dd29abdce9d7bc36a778db859c4f88b92e3d
Raymond Kao <rakao@microsoft.com>
2017-03-05 10:42:32 -0500

    launching script from a new shell script

--- bash/09.test.sh
+++ bash/09.test.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
 touch /tmp/strider-test.txt
+
+su strider<<'EOF'
+
+cd ~/
+
+touch test.txt
+
+git clone https://github.com/Strider-CD/strider.git && cd strider
+
+npm install
+
+EOF
