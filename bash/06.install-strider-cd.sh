bb64618815d80b987b8144c944f86c8b93c1af1c
Raymond Kao <rakao@microsoft.com>
2017-03-04 22:30:49 -0500

    tested script #06 directly on VM and it works in context of another user.  VMAgent doesn't seem to execute the same way

--- bash/06.install-strider-cd.sh
+++ bash/06.install-strider-cd.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 # install node
-su strider
+su strider<<'EOF'
 
 cd ~/
 
-touch test.txt
+git clone https://github.com/Strider-CD/strider.git && cd strider
+
+npm install
+
+EOF
