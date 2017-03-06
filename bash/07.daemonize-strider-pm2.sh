435bb642a6078645a087a9632d08dcf7955ec0ed
Raymond Kao <rakao@microsoft.com>
2017-03-05 22:57:19 -0500

    updated daemonize script for strider. runs the pm2 start process as strider then inits the systemd script as root with sudo

--- bash/07.daemonize-strider-pm2.sh
+++ bash/07.daemonize-strider-pm2.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
-env PATH=$PATH:/usr/local/lib/node-v6.10.0-linux-x64/bin pm2 startup systemd -u strider --hp /home/strider
+su strider<<'EOF'
+
+cd ~/strider
+
+pm2 start bin/strider
+
+exit
+
+EOF
+
+sudo env PATH=$PATH:/usr/local/lib/node-v6.10.0-linux-x64/bin pm2 startup systemd -u strider --hp /home/strider
+
+pm2 save
 
 shutdown -r -t now
